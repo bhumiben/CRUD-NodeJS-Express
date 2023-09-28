b5aa67d22941699f5dd41d4dab6d24691ea50825
Bhumi <bhumigoswami128@gmail.com>
2023-09-27 20:18:29 -0400

    Implement

--- fileOperations.js
+++ fileOperations.js
@@ -1,51 +1,43 @@
 const fs = require('fs');
-const path = require('path'); // Import the 'path' module
 
 // Function to handle file read operations
-function readFile(filePath) {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading the file:', err);
-            return;
-        }
-        console.log('File content:', data);
-    });
+function readFile(filePath, callback) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading the file:', err);
+      return callback(err, null);
+    }
+    callback(null, data);
+  });
 }
 
 // Function to handle file write operations
-function writeFile(filePath, data) {
-    fs.writeFile(filePath, data, (err) => {
-        if (err) {
-            console.error('Error writing the file:', err);
-            return;
-        }
-        console.log('The file has been saved!', data);
-    });
+function writeFile(filePath, data, callback) {
+  fs.writeFile(filePath, data, (err) => {
+    if (err) {
+      console.error('Error writing the file:', err);
+      return callback(err);
+    }
+    console.log('The file has been saved!', data);
+    callback(null);
+  });
 }
 
 // Function to handle file deletion
-function deleteFile(filePath) {
-    fs.unlink(filePath, (err) => {
-        if (err) {
-            console.error('Error deleting a file', err);
-            return;
-        }
-        console.log(`${filePath} was deleted`);
-    });
+function deleteFile(filePath, callback) {
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.error('Error deleting a file', err);
+      return callback(err);
+    }
+    console.log(`${filePath} was deleted`);
+    callback(null);
+  });
 }
 
-// Example usage:
-const fileName = 'week1.txt'; // Rename this variable for clarity
-const filePath = path.join(__dirname, fileName); // Construct the full file path
-
-const data = 'bhumi';
-
-// Read the file
-readFile(filePath);
-
-// Write data to the file
-writeFile(filePath, data);
-
-// Delete the file
-deleteFile(filePath);
+module.exports = {
+  readFile,
+  writeFile,
+  deleteFile,
+};
 
