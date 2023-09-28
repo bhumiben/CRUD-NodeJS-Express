d5d27c7066858da34394f4df7a8bcb64bc35226e
Bhumi <bhumigoswami128@gmail.com>
2023-09-27 20:30:44 -0400

    Implement1

--- server.js
+++ server.js
@@ -1,59 +1,82 @@
 const http = require('http');
-const fs = require('fs');
 const path = require('path');
-
-// Importing file operation functions from fileOperations.js
 const { readFile, writeFile, deleteFile } = require('./fileoperations');
 
-// Creating an HTTP server
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/api/v1/users') {
-    // Read user data from a file using fs and path modules
-    fs.readFile(path.join(__dirname, 'users.json'), 'utf8', (err, data) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-        return;
-      }
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(data);
-    });
-  } else if (req.method === 'POST' && req.url === '/api/v1/users') {
-    // Handleing and creating a new user 
-    let body = '';
+// Define API endpoints
+const apiEndpoints = {
+  GET_USERS: '/api/v1/users',
+  POST_USERS: '/api/v1/users',
+  DELETE_USERS: '/api/v1/users',
+};
 
-    req.on('data', (chunk) => {
-      body += chunk;
-    });
+// Handle GET request to retrieve user data
+function handleGetUsers(req, res) {
+  readFile(path.join(__dirname, 'users.json'), (err, data) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(data);
+  });
+}
 
-    req.on('end', () => {
-      // Parseing the JSON data
-      try {
-        const newUser = JSON.parse(body);
+// Handle POST request to create a new user
+function handlePostUsers(req, res) {
+  let body = '';
 
-        // Write the new user data to the JSON file
-        writeFile(path.join(__dirname, 'users.json'), newUser);
+  req.on('data', (chunk) => {
+    body += chunk;
+  });
 
+  req.on('end', () => {
+    try {
+      const newUser = JSON.parse(body);
+      writeFile(path.join(__dirname, 'users.json'), newUser, (err) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Internal Server Error');
+          return;
+        }
         res.writeHead(201, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(newUser));
-      } catch (err) {
-        res.writeHead(400, { 'Content-Type': 'text/plain' });
-        res.end('Bad Request');
-      }
-    });
-  } else if (req.method === 'DELETE' && req.url === '/api/v1/users') {
-    // Handling deleting the user data file
-    deleteFile(path.join(__dirname, 'users.json'));
+      });
+    } catch (err) {
+      res.writeHead(400, { 'Content-Type': 'text/plain' });
+      res.end('Bad Request');
+    }
+  });
+}
 
+// Handle DELETE request to delete user data
+function handleDeleteUsers(req, res) {
+  deleteFile(path.join(__dirname, 'users.json'), (err) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+      return;
+    }
     res.writeHead(204);
     res.end();
+  });
+}
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === apiEndpoints.GET_USERS) {
+    handleGetUsers(req, res);
+  } else if (req.method === 'POST' && req.url === apiEndpoints.POST_USERS) {
+    handlePostUsers(req, res);
+  } else if (req.method === 'DELETE' && req.url === apiEndpoints.DELETE_USERS) {
+    handleDeleteUsers(req, res);
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
   }
 });
 
-// Starting the server on port 3000
+// Start the server on port 3000
 server.listen(3000, () => {
   console.log('Server running on <http://localhost:3000/>');
 });
