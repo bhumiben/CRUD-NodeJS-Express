6b55ec8b17f2254729024e7b07cfb14bda2957fb
Bhumi <bhumigoswami128@gmail.com>
2023-11-25 14:33:27 -0500

    Integrate Mongoose and schemas

--- server.js
+++ server.js
@@ -1,37 +1,32 @@
 import express from 'express';
-import { MongoClient } from 'mongodb';
+import mongoose from 'mongoose';
 import methodOverride from 'method-override';
 
 const app = express();
 const port = 3000;
 
+mongoose.connect('mongodb://127.0.0.1', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB.'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
 
-const uri = "mongodb://127.0.0.1";
-let db;
+const userSchema = new mongoose.Schema({
+    name: String
+});
 
-(async function() {
-    try {
-        const client = await MongoClient.connect(uri, { useUnifiedTopology: true });
-        console.log('Connected to MongoDB.');
-        db = client.db("bhumi");
-
-    } catch (err) {
-        console.error('Error occurred while connecting to MongoDB:', err);
-    }
-})();
-
+const User = mongoose.model('User', userSchema);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-
-
 app.set('views', 'views');
 app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
-    res.send(' ');
+    res.send('Welcome to the root route. ');
 });
 
 
@@ -43,43 +38,33 @@ app.get('/addUser', (req, res) => {
 app.post('/api/v1/users', async(req, res) => {
     const { name } = req.body;
 
-    const newUser = {
-        name,
-    };
-
-    
-    const collection = db.collection('users');
-    collection.insertOne(newUser, (err, result) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send('Error saving to database');
-            return;
-        }
-
-        if (result.ops && result.ops.length > 0) {
-            console.log('Saved to database:', result.ops[0]);
-        } else {
-            console.log('Saved to database, but ops array is empty.');
-        }
+    const newUser = new User({
+        name
     });
+    try{
+    const savedUser = await newUser.save();
+    console.log('Saved to database:', savedUser);
 
     res.redirect('/api/v1/users');
+  } catch (err) {
+    console.error('Error saving to database:', err);
+    res.status(500).send('Error saving to database');
+  }
 });
 
-
-
+// GET route using Mongoose Model
 app.get('/api/v1/users', async (req, res) => {
-    try {
-        const collection = db.collection('users');
-        const users = await collection.find({}).toArray();
-        res.render("users", { users });
-    } catch (err) {
-        console.log(err);
-        res.status(500).send('Error fetching from database');
-    }
+  try {
+    // Fetch all users from the database using the Mongoose model
+    const users = await User.find({});
+    res.render('users', { users });
+  } catch (err) {
+    console.error('Error fetching from database:', err);
+    res.status(500).send('Error fetching from database');
+  }
 });
-
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}/`);
+  console.log(`Server running on http://localhost:${port}/`);
 });
+
